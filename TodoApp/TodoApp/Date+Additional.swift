4d58a4cc304e7f0e966c5966d7142223353e9f46
SoKoL-ll <sokolov-19.07@yandex.ru>
2023-06-17 00:52:44 +0300

    Add TodoItem, FileCache, Date+Additional. Update Unit tests

--- TodoApp/TodoApp/Date+Additional.swift
+++ TodoApp/TodoApp/Date+Additional.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+extension Date {
+    enum DateFormat: String {
+        case simpleFormat = "dd.MM.yyyy"
+        var format: String {
+            return self.rawValue
+        }
+    }
+    
+    static func toDate(from string: String, with format: DateFormat = .simpleFormat) -> Date? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format.format
+
+        return dateFormatter.date(from: string)
+    }
+    
+    func toString(with format: DateFormat = .simpleFormat) -> String {
+        let components = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: self)
+        var result = format.format
+        zip(["dd", "MM", "yyyy", "HH", "mm", "ss"], [components.day, components.month, components.year, components.hour, components.minute, components.second]).forEach { format, dateComponents in
+            result = result.replacingOccurrences(of: format, with: "\(dateComponents ?? 0)")
+        }
+        
+        return result
+    }
+}
--- TodoApp/TodoAppTests/TodoAppTests.swift
+++ TodoApp/TodoAppTests/TodoAppTests.swift
@@ -10,27 +10,126 @@ import XCTest
 
 final class TodoAppTests: XCTestCase {
 
+    var todoItem: TodoItem!
     override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        todoItem = TodoItem(id: "4", text: "купить машину", importance: Importance.unimportant, deadline: Date.toDate(from: "11.11.2002") ?? Date(), isDone: true, creationDate: Date.toDate(from: "11.12.2002") ?? Date(), modifiedDate: Date.toDate(from: "11.10.2002") ?? Date())
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        todoItem = nil
+        try super.tearDownWithError()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testInit() throws {
+        XCTAssertEqual(self.todoItem.id, "4")
+        XCTAssertEqual(self.todoItem.text, "купить машину")
+        XCTAssertEqual(self.todoItem.importance, Importance.unimportant)
+        XCTAssertEqual(self.todoItem.deadline, Date.toDate(from: "11.11.2002")!)
+        XCTAssertTrue(self.todoItem.isDone)
+        XCTAssertEqual(self.todoItem.creationDate, Date.toDate(from: "11.12.2002")!)
+        XCTAssertEqual(self.todoItem.modifiedDate, Date.toDate(from: "11.10.2002")!)
+        
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testParseJson() throws {
+        var answer: [String: Any] = [
+            "text": "купить машину",
+            "isDone": true,
+            "id": "4",
+            "creationDate": "11.12.2002",
+            "importance": "unimportant",
+            "modifiedDate": "11.10.2002",
+            "deadline": "11.11.2002"
+        ]
+        
+        var todoItemFromJson = TodoItem.parse(json: answer)
+        
+        XCTAssertEqual(self.todoItem.id, todoItemFromJson?.id)
+        XCTAssertEqual(self.todoItem.text, todoItemFromJson?.text)
+        XCTAssertEqual(self.todoItem.importance, todoItemFromJson?.importance)
+        XCTAssertEqual(self.todoItem.deadline, todoItemFromJson?.deadline)
+        XCTAssertEqual(self.todoItem.isDone, todoItemFromJson?.isDone)
+        XCTAssertEqual(self.todoItem.creationDate, todoItemFromJson?.creationDate)
+        XCTAssertEqual(self.todoItem.modifiedDate, todoItemFromJson?.modifiedDate)
+        
+        answer["id"] = nil
+        todoItemFromJson = TodoItem.parse(json: answer)
+        
+        XCTAssertEqual(nil, todoItemFromJson?.isDone)
+        
+        answer = [
+            "text": "купить машину",
+            "id": "4",
+            "creationDate": "",
+            "modifiedDate": "11.10.2002",
+            "deadline": "11.11.2002"
+        ]
+        
+        todoItemFromJson = TodoItem.parse(json: answer)
+        
+        XCTAssertEqual(todoItemFromJson?.isDone, false)
+        XCTAssertEqual(todoItemFromJson?.importance, Importance.common)
     }
+    
+    
+    func testGetJsonFromTodoItem() throws {
+        let answer: [String: Any] = [
+            "text": "купить машину",
+            "isDone": true,
+            "id": "4",
+            "creationDate": "11.12.2002",
+            "importance": "unimportant",
+            "modifiedDate": "11.10.2002",
+            "deadline": "11.11.2002"
+        ]
+        let todoItemJson = self.todoItem.json as? [String: Any]
+        XCTAssertEqual(todoItemJson!["id"] as! String, answer["id"] as! String)
+        XCTAssertEqual(todoItemJson!["text"] as! String, answer["text"] as! String)
+        XCTAssertEqual(todoItemJson!["isDone"] as! Bool, answer["isDone"] as! Bool)
+        XCTAssertEqual(todoItemJson!["creationDate"] as! String, answer["creationDate"] as! String)
+        XCTAssertEqual(todoItemJson!["importance"] as! String, answer["importance"] as! String)
+    }
+    
+    func testGetCSVFromTodoItem() throws {
+        let answer = "4,купить машину,unimportant,11.11.2002,true,11.12.2002,11.10.2002"
+        
+        let todoItemCSV = self.todoItem.csv
+        
+        XCTAssertEqual(answer, todoItemCSV)
+    }
+    
+    func testParseCSV() throws {
+        var answer = "4,купить машину,unimportant,11.11.2002,true,11.12.2002,11.10.2002"
+        
+        var todoItemFromCSV = TodoItem.parse(csv: answer)
+        
+        XCTAssertEqual(self.todoItem.id, todoItemFromCSV?.id)
+        XCTAssertEqual(self.todoItem.text, todoItemFromCSV?.text)
+        XCTAssertEqual(self.todoItem.importance, todoItemFromCSV?.importance)
+        XCTAssertEqual(self.todoItem.deadline, todoItemFromCSV?.deadline)
+        XCTAssertEqual(self.todoItem.isDone, todoItemFromCSV?.isDone)
+        XCTAssertEqual(self.todoItem.creationDate, todoItemFromCSV?.creationDate)
+        XCTAssertEqual(self.todoItem.modifiedDate, todoItemFromCSV?.modifiedDate)
+        
+        answer = "4,купить машину,,11.11.2002,true,11.12.2002,11.10.2002"
+        todoItemFromCSV = TodoItem.parse(csv: answer)
+        XCTAssertEqual(Importance.common, todoItemFromCSV?.importance)
+        
+        answer = "4,11.10.2002"
+        todoItemFromCSV = TodoItem.parse(csv: answer)
+        XCTAssertEqual(nil, todoItemFromCSV?.id)
 
+        answer = "4,купить машину,unimportant,,,11.12.2002,"
+        todoItemFromCSV = TodoItem.parse(csv: answer)
+        XCTAssertEqual(nil, todoItemFromCSV?.deadline)
+        XCTAssertEqual(nil, todoItemFromCSV?.modifiedDate)
+        
+        
+        
+    }
 }
